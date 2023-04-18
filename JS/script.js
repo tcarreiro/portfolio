94ec718a1cb5663c33146c44b3a32173a3836ebb
Thiago <thiago.carreiro@hotmail.com>
2023-04-18 13:05:57 -0300

    CSS and JS animations

--- JS/script.js
+++ JS/script.js
@@ -8,11 +8,26 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
+const tabs = document.querySelectorAll('.navbar a');
+const highlight = document.querySelector('.highlight');
+const firstTab = tabs[0];
+
+attHighlight(firstTab);
+
+function attHighlight(activeLink) {
+    let leftTab = activeLink.offsetLeft;
+    let widthTab = activeLink.offsetWidth;
+    
+    highlight.style.left = `${leftTab}px`;
+    highlight.style.width = `${widthTab}px`;
+};
+
+tabs.forEach(tab => {tab.addEventListener("click", attHighlight(this))});
+
 /* ======================= Header active link ======================= */
 
 let sections = document.querySelectorAll('section')
 let navLinks = document.querySelectorAll('header nav a')
-
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -26,6 +41,8 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id +']').classList.add('active');
             });
+            let activeLink = document.querySelector('header nav a[href*=' + id +']');
+            attHighlight(activeLink);
         };
     });
 
@@ -34,3 +51,36 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 
 };
+
+window.addEventListener("resize", function() {
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+            let activeLink = document.querySelector('header nav a[href*=' + id +']');
+            attHighlight(activeLink);
+        };
+    });
+});
+
+/* ======================= scroll ======================= */
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin:'top'});
+ScrollReveal().reveal('.home-img, .about-img, .portfolio-box, .contact form', {origin:'bottom'});
+ScrollReveal().reveal('.home-content h1', {origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
